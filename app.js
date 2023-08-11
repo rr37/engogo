62c0fd04d768d99e80ade32ee86c0822d478040a
rr37 <ay870527@gmail.com>
2023-08-11 20:48:59 +0800

    build: add express-handlebars & hbs-helper

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-// app.js
 // 載入express
 const express = require('express')
-
 // 建構應用程式伺服器
 const app = express()
+
+// 引用 hbs
+const exphbs = require('express-handlebars')
+// 引用 helpers
+const helpers = require('handlebars-helpers')()
+
+// 設定 helpers
+app.engine('hbs', 
+  exphbs({ 
+    helpers: helpers,
+    defaultLayout: 'main', 
+    extname: '.hbs' 
+  })
+)
+
+app.set('view engine', 'hbs')
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -14,7 +28,7 @@ const PORT = process.env.PORT
 
 // 設定路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定監聽運行訊息
