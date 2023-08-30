b32acbdcda11515df112b5cd6bb345958c77468d
rr37 <ay870527@gmail.com>
2023-08-30 13:40:48 +0800

    feat: add userMissionCardsPage

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -10,25 +10,44 @@ const {
 const userController = {
   getUserJournalsPage: (req, res, next) => {
     const userId = req.params.id
-      User.findByPk(userId, {
-        include: [
-          {
-            model: Journal,
-            include: [{ model: MissionCard, include: [{ model: CardImage }] }],
-            nest: true,
-            
-          },
-        ],
-        nest: true,
+    User.findByPk(userId, {
+      include: [
+        {
+          model: Journal,
+          include: [{ model: MissionCard, include: [{ model: CardImage }] }],
+          nest: true,
+        },
+      ],
+      nest: true,
+    })
+      .then((userData) => {
+        // console.log(JSON.stringify(userData, null, 2))
+        userData = userData.toJSON()
+        const userJournals = userData.Journals
+        console.log(userJournals)
+        res.render('userPage', { userData, userJournals })
       })
-        .then((userData) => {
-          // console.log(JSON.stringify(userData, null, 2))
-          userData = userData.toJSON()
-          const userJournals = userData.Journals
-          console.log(userJournals)
-          res.render('userPage', { userData, userJournals })
-        })
-        .catch((err) => next(err))
+      .catch((err) => next(err))
+  },
+  getUserMissionCardsPage: (req, res, next) => {
+    const userId = req.params.id
+    User.findByPk(userId, {
+      include: [
+        {
+          model: Journal,
+          include: [{ model: MissionCard, include: [{ model: CardImage }] }],
+          nest: true,
+        },
+      ],
+      nest: true,
+    })
+      .then((userData) => {
+        // console.log(JSON.stringify(userData, null, 2))
+        userData = userData.toJSON()
+        const userMissionCards = userData.Journals
+        res.render('userPage', { userData, userMissionCards })
+      })
+      .catch((err) => next(err))
   },
 }
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/user-controller')
 
-// 個別 user 的頁面
+// 個別 user 的感恩日記本頁面
 router.get('/:id/journals', userController.getUserJournalsPage)
+
+// 個別 user 的牌卡搜集庫頁面
+router.get('/:id/missioncards', userController.getUserMissionCardsPage)
 
 router.get('/:id', (req, res, next) =>
   res.redirect(`./${req.params.id}/journals`)
